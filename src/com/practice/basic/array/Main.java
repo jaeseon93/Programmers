ae146e0aba4d55371567f684d5e1f6b78f8cae37
jaeseon93 <seon2723@gmail.com>
2021-09-16 01:59:38 +0900

    배열 숫자, 하나씩 더하기

---
+++ src/com/practice/basic/array/Main.java
@@ -0,0 +1,9 @@
+package com.practice.basic.array;
+
+public class Main {
+    public static void main(String[] args) {
+        ArraySum arr = new ArraySum();
+        int solution2 = arr.solution(123);
+        System.out.println("solution2 :"+ solution2);
+    }
+}
