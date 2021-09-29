e5bb3fae63a7bb1197123298d3892207db590aa5
jaeseon93 <seon2723@gmail.com>
2021-09-29 17:52:25 +0900

    콜라츠 추측

--- src/com/practice/test/Main.java
+++ src/com/practice/test/Main.java
@@ -3,8 +3,17 @@ package com.practice.test;
 public class Main {
     public static void main(String[] args) {
 
+        NumSum arr = new NumSum();
+        int arraySum = arr.solution(123);
+        System.out.println("ArraySum :"+ arraySum);
+
         ReadNumberToEng solution6 = new ReadNumberToEng();
         String numberToEng = solution6.solution(149);
         System.out.println("numberToEng :" + numberToEng);
+
+        // 콜라츠 추측
+        ResultMinusOne minusOne = new ResultMinusOne();
+        int solution = minusOne.solution(1);
+        System.out.println("solution :" + solution);
     }
 }
